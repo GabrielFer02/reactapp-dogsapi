d1b1e4b8f59e81ed940b3bd6cd7c7c979939b8fc
Gabriel Fernando <gf313074@gmail.com>
2024-12-02 13:26:47 -0300

    create definitive structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Footer from "./Components/Footer";
+import Login from "./Components/Login/Login";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />}/>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 };
 
 export default App;
