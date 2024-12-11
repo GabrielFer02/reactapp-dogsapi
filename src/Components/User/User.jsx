3eef0b3916048cd064d486a1118795528eb54b65
Gabriel Fernando <gf313074@gmail.com>
2024-12-11 13:43:04 -0300

    implementation scroll infinite

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -5,12 +5,15 @@ import Feed from "../Feed/Feed";
 import UserPhotoPost from "./UserPhotoPost";
 import UserStats from "./UserStats";
 import NotFound from "../NotFound";
+import { UserContext } from "../../UserContext";
 const User = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <section className="container">
       <UserHeader />
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Feed user={data} />} />
         <Route path="postar" element={<UserPhotoPost />} />
         <Route path="estatisticas" element={<UserStats />} />
         <Route path="*" element={<NotFound />} />
